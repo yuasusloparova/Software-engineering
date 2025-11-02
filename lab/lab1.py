2c92001654b0e747328bacd1251b2babfdd67fb6
yulichka <jd124587@mail.ru>
2025-11-02 21:17:54 +0500

    Create lab1.py

---
+++ lab/lab1.py
@@ -0,0 +1,5 @@
+class Car:
+    def __init__(self, make, model): #конструктор класса, который принимает 2 параметра
+        self.make = make
+        self.model = model
+my_car = Car("Toyota", "Corolla") #создаем экземпляр класса
