69b0248ccd6f28b7bf27af9914ddf45c67bc558f
yulichka <jd124587@mail.ru>
2025-11-09 18:55:53 +0300

    Create lab3.py

---
+++ lab/lab3.py
@@ -0,0 +1,25 @@
+class MyClass:
+    def __init__(self, value):
+        self._value=value
+
+    def set_value(self, value):
+        self._value=value
+
+    def get_value(self):
+        return self._value
+    
+    def del_value(self):
+        del self._value
+
+    value = property(get_value, set_value, del_value, "свойство value")
+
+obj = MyClass(42)
+print(obj.get_value()) 
+obj.set_value(45)       
+print(obj.get_value())  
+obj.set_value(100)    
+print(obj.get_value())
+
+
+
+
