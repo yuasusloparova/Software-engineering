982a50e7a5d58f30668cc8b30bb672994bdb8d1e
yulichka <jd124587@mail.ru>
2025-11-16 15:28:34 +0300

    Create lab4.py

---
+++ lab/lab4.py
@@ -0,0 +1,12 @@
+class NegativeValueException(Exception):
+    pass
+
+def check_name(name):
+    if len(name) > 10:
+        raise NegativeValueException('Длина более 10 символов')
+    else:
+        print('Успешная регистрация')
+
+if __name__ == '__main__':
+    name = '12345678900'
+    check_name(name)
